2ce7fac707ced9fabc0fc43678efaac38ae5b6df
daniil138 <futurino120.bom@gmail.com>
2026-03-03 15:03:46 +0800

    add interface for motion

--- car_control/car_control.ino
+++ car_control/car_control.ino
@@ -19,16 +19,58 @@ void move(bool left_dir, int left_speed, bool right_dir, int right_speed){
 
 }
 
+void forward(int speed){
+  move(FORWARD, speed, FORWARD, speed );
+
+}
+
+void backword(int speed){
+  move(BACK, speed, BACK, speed );
+
+}
+
+void tern_left(int steepness){
+  move(FORWARD, steepness, FORWARD, 255);
+
+
+}
+
+void tern_right(int steepness){
+  move(FORWARD,255 , FORWARD, steepness);
+
+}
+
+void rotate_righ(int speed){
+  move(FORWARD, speed, BACK, speed );
+}
+
+void rotate_left(int speed){
+  move(BACK, speed, FORWARD, speed );
+}
+
 void setup() {
   pinMode(SPEED_RIGHT,OUTPUT);
   pinMode(DIR_RIGHT,OUTPUT);
   pinMode(SPEED_LEFT,OUTPUT);  
   pinMode(DIR_LEFT,OUTPUT);  
 
+
+
 }
 
 void loop() {
-move(BACK, 100, FORWARD, 100 );
+  forward(200);
+  delay(1000);
+  backword(200);
+  delay(1000);
+  tern_left(50);
+  delay(1000);
+  tern_right(50);
+  delay(1000);
+  rotate_left(150);
+  delay(1000);
+  rotate_righ(150);
+  delay(1000);
 
   
 
